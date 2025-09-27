ae2ef8eed128f98207eb883624664273bb5b765c
Anh Phuong Nguyen <anhphuong.nguyenquynh02@gmail.com>
2025-09-27 22:59:29 +0700

    fix bug of fast API with news categorize

--- main_news_category.py
+++ main_news_category.py
@@ -8,6 +8,7 @@ import pickle
 from model_train import BiLSTM
 from func_dataprep import preprocess_text, pad_input, map_tokens
 from func_eval_predict import predict
+from torchmetrics.classification import MulticlassAccuracy, MulticlassPrecision, MulticlassRecall
 
 
 # Load utils BiLSTM
@@ -25,7 +26,7 @@ model = BiLSTM(vocab_size=len(word2idx), embedding_dim = 300, hidden_dim=128, ou
 model.load_state_dict(torch.load("saved_model/news_categorize_bilstm.pth", map_location=torch.device("cpu")))
 model.eval()
 
-# Test sample with prediction function
+#Test sample with prediction function
 
 # sample_text = "IRS Launches Safety Review Amid Threats To Workers Linked To Conspiracy Theories"
 # label, probs = predict(model, sample_text, word2idx, max_len=30, id2label=id2label, device="cpu")
@@ -41,7 +42,7 @@ class ArticleText(BaseModel):
 
 @app.post("/news_category")
 def predict_endpoint(input: ArticleText):
-    _, label, probs = predict(model, input.header, word2idx, max_len, id2label)
+    label, probs = predict(model, input.header, word2idx, max_len, id2label)
     return {"label": label, "probabilities": probs}
 
 if __name__ == "__main__":
